d68a53c0fd27b32cf7b4644f3ce629b0370b55f3
fred-song <fred.song@containerchain.com>
2019-09-12 12:28:43 +1000

    first commit

---
+++ ASPNETCoreReact/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ASPNETCoreReact
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "clientapp/build";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+            app.UseMvc();
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = Path.Join(env.ContentRootPath, "ClientApp");
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
+            });
+        }
+    }
+}
---
+++ ASPNETCoreReact/clientapp/src/components/Form.tsx
@@ -0,0 +1,20 @@
+﻿import React from "react";
+interface IFormProps {
+    /* The http path that the form will be posted to */
+    getWeather: any;
+  }
+   
+
+class Form extends React.Component<IFormProps> {
+    render() {
+        return (
+            <form onSubmit={this.props.getWeather}>
+                <input type="text" name="city" placeholder="City..." />
+                <input type="text" name="country" placeholder="Country..." />
+                <button>Get Weather</button>
+            </form>
+        );
+    }
+};
+
+export default Form;
